2028b7db1d99a41842a8445db3986d8ed68130d4
estaeldev <estael.dev@gmail.com>
2024-06-14 16:29:45 -0300

    conteudo desenvolvido em aula

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/socialmediaapplication/UserController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/socialmediaapplication/UserController.java
@@ -1,7 +1,11 @@
 package com.in28minutes.rest.webservices.restful_web_services.socialmediaapplication;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
@@ -21,23 +26,36 @@ public class UserController {
     }
 
     @GetMapping
-    public List<User> findAll() {
-        return this.userDaoService.findAll();
+    public  ResponseEntity<List<User>> findAll() {
+        List<User> userList = this.userDaoService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(userList);
     }
 
     @GetMapping("/{id}")
-    public User findById(@PathVariable Integer id){
-        return this.userDaoService.findById(id);
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        User user = this.userDaoService.findById(id);
+
+        if(Objects.isNull(user)) {
+            throw new UserNotFoundException("User Not Found; id:"+id);
+        }
+        
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
     
     @PostMapping
-    public User save(@RequestBody User user) {
-        return this.userDaoService.save(user);
+    public ResponseEntity<User> save(@RequestBody User user) {
+        User userSaved = this.userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(userSaved.getId())
+            .toUri();
+        return ResponseEntity.created(location).build();
     }
-
+    
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Integer id) {
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         this.userDaoService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
     
     
