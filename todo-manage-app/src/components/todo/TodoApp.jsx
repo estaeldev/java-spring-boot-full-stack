c59fb2abc0f9c2b3c842623ed0737d8b7ee6ad18
estaeldev <estael.dev@gmail.com>
2024-06-26 19:11:59 -0300

    conteudo desenvolvido em aula

--- todo-manage-app/src/components/todo/TodoApp.jsx
+++ todo-manage-app/src/components/todo/TodoApp.jsx
@@ -1,8 +1,10 @@
+import { useRef, useState } from 'react'
+import './TodoApp.css'
+
 export const TodoApp = () => {
 
     return (
-        <div>
-            App Management Application
+        <div className="todoApp">
             <Login />
             {/* <Welcome /> */}
         </div>
@@ -12,20 +14,54 @@ export const TodoApp = () => {
 
 export const Login = () => {
 
+    const [isAuthentication, setIsAuthentication] = useState(null)
+    const nameRef = useRef(null)
+    const passwordRef = useRef(null)
+
+    const handleLogin = () => {
+        const username = nameRef.current.value
+        const password = passwordRef.current.value
+
+        if(username === "estael" && password === "1234") {
+            setIsAuthentication(true)
+        } else {
+            setIsAuthentication(false)
+        }
+
+    }
+
     return (
-        <div className="login">
-            
-            <div className="form">
-                <div>
-                    <label htmlFor="name">User Name</label>
-                    <input type="text" name="username" />
+        <div className="loginContainer">
+
+            <div className="login">
+                <strong>Todo Management Application</strong>
+                <div className="form">
+                    <div>
+                        <label htmlFor="name">User Name</label>
+                        <input 
+                            type="text" 
+                            name="username" 
+                            defaultValue="estael"
+                            ref={nameRef} 
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="name">Password</label>
+                        <input 
+                            type="password" 
+                            name="password" 
+                            ref={passwordRef}
+                        />
+                    </div>
+                    <button type="button" onClick={handleLogin}>login</button>
                 </div>
-                <div>
-                    <label htmlFor="name">Password</label>
-                    <input type="password" name="password" />
-                </div>
-                <button type="button">login</button>
             </div>
+            {isAuthentication === true && (
+                <span className="successMessage">Authentication Successfully</span>
+            )}
+            {isAuthentication === false && (
+                <span className="errorMessage">Authentication Failed. Please check your credentials</span>
+            )}
 
         </div>
     )
