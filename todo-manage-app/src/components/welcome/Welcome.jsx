0b51df618f14d1513ce6530031ef092c76e7a46d
estaeldev <estael.dev@gmail.com>
2024-06-29 18:27:31 -0300

    conteudo desenvolvido em aula

--- todo-manage-app/src/components/welcome/Welcome.jsx
+++ todo-manage-app/src/components/welcome/Welcome.jsx
@@ -1,8 +1,21 @@
+import axios from "axios"
+import { useState } from "react"
 import { Link, useParams } from "react-router-dom"
 
 export const Welcome = () => {
 
     const {username} = useParams()
+    const [message, setMessage] = useState(null)
+
+    const handleHelloWorldRestApi = () => {
+        axios.get("http://localhost:8080/hello-world-bean")
+            .then(result => {
+                setMessage(result.data.message)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
 
     return (
         <div className="container d-flex justify-content-center">
@@ -12,6 +25,16 @@ export const Welcome = () => {
                 <div>
                     Manager your todos - <Link to={"/todos"}>Go here</Link>
                 </div>
+
+                <div>
+                    <span className="text-info">{message}</span>
+                    <button 
+                        className="btn btn-primary btn-sm mt-5" 
+                        onClick={handleHelloWorldRestApi}>
+                            Call Hello World REST API
+                    </button>
+                </div>
+
             </div>
 
 
