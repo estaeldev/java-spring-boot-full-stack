3f15f8157d7eb786ecb9f0cc387d5dcaab8f6385
estaeldev <estael.dev@gmail.com>
2024-07-15 18:27:29 -0300

    conteudo desenvolvido em aula

--- learn-spring-security/src/main/java/com/project/learnspringsecurity/config/SpringSecurityConfig.java
+++ learn-spring-security/src/main/java/com/project/learnspringsecurity/config/SpringSecurityConfig.java
@@ -1,7 +1,12 @@
 package com.project.learnspringsecurity.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -11,9 +16,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,7 +31,15 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    UserDetailsService userDetailsService() {
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+            .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(DataSource dataSource) {
         UserDetails user = User.builder()
             .username("estael")
             .password("1234")
@@ -39,8 +53,12 @@ public class SpringSecurityConfig {
             .roles("ADMIN")
             .passwordEncoder(password -> passwordEncoder().encode(password))
             .build();
+        
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
 
-        return new InMemoryUserDetailsManager(user, admin);
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -52,13 +70,18 @@ public class SpringSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers(PathRequest.toH2Console()).permitAll()
+            .anyRequest().authenticated()
+        );
         http.httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.headers(header -> header.frameOptions(Customizer.withDefaults()).disable());
 
         return http.build();
     }
 
+    
     
 }
